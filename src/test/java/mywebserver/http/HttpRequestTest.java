dc6799d351a6837f4ef74d6f0ecc59318059c38e
JYL35 <dlwodbs1223@naver.com>
2025-11-16 15:42:06 +0900

    test(HttpRequestTest): Http 시작 라인 파싱 테스트 작성

--- src/test/java/mywebserver/http/HttpRequestTest.java
+++ src/test/java/mywebserver/http/HttpRequestTest.java
@@ -1,5 +1,6 @@
 package mywebserver.http;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
@@ -7,6 +8,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class HttpRequestTest {
@@ -20,6 +22,22 @@ public class HttpRequestTest {
         assertThatThrownBy(() -> new HttpRequest(in))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("유효하지 않은 시작 라인입니다");
+    }
+
+    @DisplayName("메소드와 경로를 올바르게 파싱한다")
+    @ParameterizedTest
+    @CsvSource({
+            "GET /index.html HTTP/1.1, GET, /index.html",
+            "POST / HTTP/1.1, POST, /"
+    })
+    void HttpRequest_ParseStartLine_ReturnsMethodAndPath(String startLine, String httpMethod, String path) {
+        String requestString = startLine + "\r\n" + "Host: localhost:8080\r\n" + "\r\n";
+        InputStream in = createRequest(requestString);
+
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod()).isEqualTo(httpMethod);
+        assertThat(request.getPath()).isEqualTo(path);
     }
 
     private InputStream createRequest(String requestString) {
