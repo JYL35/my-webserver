bcf5487d006d949729894569a7ccef9589027d64
JYL35 <dlwodbs1223@naver.com>
2025-11-18 18:24:11 +0900

    feat(HttpHeaders): HTTP 헤더 파싱 기능 구현

---
+++ src/main/java/mywebserver/http/HttpHeaders.java
@@ -0,0 +1,33 @@
+package mywebserver.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import mywebserver.util.ErrorMessage;
+
+public class HttpHeaders {
+    private final Map<String, String> headers;
+
+    public HttpHeaders(BufferedReader reader) {
+        try {
+            headers = new HashMap<>();
+            parseHeaders(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.HTTP_HEADER_PARSING_ERROR
+                    .getMessage(), e);
+        }
+    }
+
+    private void parseHeaders(BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null && !line.isBlank()) {
+            String[] tokens = line.split(": ", 2);
+            headers.put(tokens[0],tokens[1]);
+        }
+    }
+
+    public String get(String key) {
+        return headers.get(key);
+    }
+}
--- src/main/java/mywebserver/util/ErrorMessage.java
+++ src/main/java/mywebserver/util/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     HTTP_METHOD_INVALID("유효하지 않은 HTTP 메소드입니다."),
     HTTP_PATH_INVALID("유효하지 않은 경로입니다."),
     HTTP_START_LINE_INCORRECT("유효하지 않은 HTTP 시작 라인입니다."),
-    HTTP_REQUEST_FAILED("HTTP 요청 실패");
+    HTTP_REQUEST_FAILED("HTTP 요청 실패"),
+    HTTP_HEADER_PARSING_ERROR("헤더 파싱 중 오류");
 
     private final String message;
 
