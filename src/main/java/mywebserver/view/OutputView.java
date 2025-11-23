3a57ffa1654f67263eaf525e464d8df6d0f46fb9
JYL35 <dlwodbs1223@naver.com>
2025-11-23 18:44:09 +0900

    feat(OutputView): 출력 클래스 구현

---
+++ src/main/java/mywebserver/view/OutputView.java
@@ -0,0 +1,17 @@
+package mywebserver.view;
+
+public class OutputView {
+
+    public void printServerStart(int port) {
+        printMessage("웹서버가 " + port + " 포트에서 시작되었습니다.");
+        printMessage("http://localhost:" + port + " 로 접속해 보세요.");
+    }
+
+    public void printException(String message, Exception e) {
+        System.err.println(message + ": " + e.getMessage());
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+}
